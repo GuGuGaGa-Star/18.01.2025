6793674543e4c8edef9bdae61aa07ec6dfd11c6f
GuGuGaGa-Star <nik.21daren@gmail.com>
2025-01-18 15:29:02 +0200

    Add files via upload

---
+++ qss.py
@@ -0,0 +1,31 @@
+QSS = '''
+QWidget#mainWindow {
+    background-color: black;  /* Задаємо фон для головного вікна */
+}
+
+QWidget {
+    font: 20px "MS Serif";  /* Встановлюємо шрифт для всіх віджетів */
+    color:white;
+}
+
+QPushButton { 
+    background-color: #white;  /* Задаємо фон для кнопки */
+    color: white;  /* Колір тексту на кнопці */
+}
+
+QPushButton:pressed {
+    background-color: #9e93fc;  /* Задаємо фон кнопки, коли вона натиснута */
+}
+
+QListWidget {
+    background-color: gray;  /* Задаємо фон для списку */
+}
+
+QListWidget::item:selected {
+    background-color: #d9ffee;  /* Задаємо фон для вибраного елемента списку */
+}
+
+Background: {
+Color: gray */
+}
+'''
